ddafb1e080f4ca46a1dbd585f7c38dd636a04702
WaghmodeS <swaghmode480@gmail.com>
2022-04-09 14:05:03 +0530

    Studt portal project

---
+++ studportal/dashboard/forms.py
@@ -0,0 +1,44 @@
+from dataclasses import fields
+# from tkinter import Widget
+from django.forms import widgets
+from xmlrpc.client import DateTime
+from django import forms
+from . models import *
+from django.contrib.auth.forms import UserCreationForm
+
+
+class NotesForm(forms.ModelForm):
+    class Meta:
+        model=Notes
+        fields=['title','description']
+        
+        
+        
+        
+        
+class DateInput(forms.DateInput):
+    input_type = 'date'       
+        
+class HomeworksForm(forms.ModelForm):
+    class Meta:
+        model=Homework
+        widgets={'due':DateInput()}
+        fields=['subject','title','description','due','is_finished']
+
+
+
+    
+class DashboardForm(forms.Form):
+    text=forms.CharField(max_length=100,label="Enter your search") 
+    
+class TodoForm(forms.ModelForm):
+  class Meta:
+    model = Todo
+    fields = ['title','is_finished']
+    
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields=['username','password1','password2',]
+    
